a22d6707b07437ceec61c34181c18aaa02484ccd
md-Imteyaz <98022042+md-Imteyaz@users.noreply.github.com>
2022-07-28 11:05:36 +0530

    Add files via upload

---
+++ EmailSenderService.java
@@ -0,0 +1,25 @@
+package com.sendingemail.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+
+	@Autowired
+	private JavaMailSender javamailsender;
+
+	public void sendemail(String toEmail, String subject, String body) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("imteyazmd143@gmail.com");
+		message.setTo(toEmail);
+		message.setSubject(subject);
+		message.setText(body);
+
+		javamailsender.send(message);
+		System.out.println("message send succesfully");
+
+	}
+}
